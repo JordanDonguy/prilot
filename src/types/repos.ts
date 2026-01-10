cac5b42775c5249d71d7a1656ad444b941388d5a
JordanDonguy <jordan.donguy@gmail.com>
2026-01-10 15:42:20 +0100

    chore(types): improve IRepository interface

--- src/types/repos.ts
+++ src/types/repos.ts
@@ -4,17 +4,19 @@ export interface IRepository {
 	id: string;
 	name: string;
 	provider: "github" | "gitlab";
-	providerRepoId: string;
 	owner: string;
-	isPrivate: boolean;
 	defaultBranch: string;
-  draftPrCount: number;
-  sentPrCount: number;
+	installationId: string;
+	createdAt: Date;
+	userRole: "owner" | "member";
+	isPrivayte: boolean;
+	draftPrCount: number;
+	sentPrCount: number;
 }
 
 export interface IRepositoryResponse {
 	repository: IRepository;
 	branches: string[];
 	pullRequests: IPullRequest[];
-  commitsCount: number;
+	commitsCount: number;
 }
